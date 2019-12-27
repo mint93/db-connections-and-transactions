67e8a19c47f48abbbf603c934c26530d6931eef0
Bartosz Nabrdalik <bartosz.nabrdalik@gmail.com>
2019-12-27 15:05:57 +0100

    Added tests for datasource logging proxy

---
+++ src/test/java/com/jdbc/connectionsAndTransactions/DatasourceLoggingProxyTests.java
@@ -0,0 +1,44 @@
+package com.jdbc.connectionsAndTransactions;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.jdbc.connectionsAndTransactions.repository.ItemRepository;
+import com.jdbc.connectionsAndTransactions.util.Util;
+import com.p6spy.engine.spy.P6DataSource;
+
+@DataJpaTest
+@TestPropertySource("classpath:h2-test-db.properties")
+public class DatasourceLoggingProxyTests {
+
+	private ItemRepository itemRepository = new ItemRepository();
+	
+	// SingleLineFormat (by default):
+	// current time|execution time|category|connection id|statement SQL String|effective SQL string
+	@Test
+	public void whenUpdatedRowsCount_ForOldVersionIsZero_ThenExceptionShouldBeThrown() throws SQLException {
+		DataSource datassource = getDataSource();
+		try (Connection conn = datassource.getConnection()) {
+			itemRepository.createTable(conn);
+			itemRepository.save(conn, "item1");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private DataSource getDataSource() {
+		JdbcDataSource datasource = new JdbcDataSource();
+		datasource.setURL(Util.connectionUrl);
+		datasource.setUser("sa");
+		return new P6DataSource(datasource);
+	}
+	
+}
